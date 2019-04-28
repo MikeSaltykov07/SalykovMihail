a9c278c60c189894cfee1f13967094632b63394d
mss <mouse-saltykov@mail.ru>
2019-04-28 19:20:07 +0300

    pj1 was done

--- Запросы.sql
+++ Запросы.sql
@@ -1,3 +1,23 @@
+-- 1. Заполнить таблицы данными, необходимыми для наглядного выполнения запросов
+-- "/бд.txt"
+-- 2. Написать запрос на каждую таблицу, который добавляет в неё данные (исключая атрибут, который заполняется при помощи автоинкремента)
+INSERT INTO Album(`Id_Album`,`Id_collectives`,`Id_group`,`Name`,`Year`) VALUES (1, 1, 8, 'Please Please Me', 1963);
+INSERT INTO Album_Compositions(`Id_album`, `Id_compositions`) VALUES (1, 'I Saw Her Standing There', 2*60+54);
+INSERT INTO Album_Genre(`Id_album`, `Id_genres`) VALUES (1,1);
+INSERT INTO Album_Label(`Id_album`, `Id_label`) VALUES (1,1);
+INSERT INTO Author (`Id_Author`, `Id_compositions`) VALUES (1,1);
+INSERT INTO Collectives (`Id_Collectives`, `Name`, `Info`) VALUES (1,'The Beatles','Британская рок-группа из Ливерпуля');
+INSERT INTO Collectives_Genres (`Id_collectives`, `Id_genres`) VALUES (1,1);
+INSERT INTO Compositions (`Id`, `Name`, `Duration`) VALUES (1, 'I Saw Her Standing There', 2*60+54);
+INSERT INTO Compositions_Genres (`Compositions_Id`, `Genres_Id`) VALUES (1,1);
+INSERT INTO Genres (`Id`, `Name`) VALUES (5, 'Рок-Н-Ролл');
+INSERT INTO `Group` (`Id_Group`, `Id_collectives`, `Date_Start`, `Date_Finish`) VALUES (8, 1,'1962-08-16','1969-09-26');
+INSERT INTO Group_People (`Id_group`, `Id_people`) VALUES (1,7);
+INSERT INTO Label (`Id`, `Name`) VALUES (1, 'Parlophone');
+INSERT INTO People (`Id_People`, `Nickname`, `Name`, `Surname`) VALUES (2, 'Пол Макка́ртни', 'Джеймс Пол', 'Макка́ртни');
+INSERT INTO People_Roles (`Id_People`, `Id_Roles`) VALUES (2,1);
+INSERT INTO Roles (`Id`, `Name`) VALUES (1, 'Вокал');
+
 -- 3. Написать запрос на каждую таблицу, который возвращает запись с заданными ID
 select * from Album where Id_Album=1;
 select * from Album_Compositions where Id_compositions=4;
@@ -50,17 +70,18 @@ delete from Roles where id=5;
 
 -- 6. Выведите группу/ы, состав которой менялся чаще всего
 select Id_collectives
-from (
-select Id_collectives, count(*) as `count`
-from `Group`
-group by   Id_collectives
-) t
+ from (
+      select Id_collectives, count(*) as `count`
+       from `Group`
+      group by   Id_collectives
+ ) t
 where t.`count` = (
-select count(*)
-from `Group`
-group by  Id_collectives
-limit 1
+                   select count(*)
+                    from `Group`
+                   group by  Id_collectives
+                   limit 1
 );
+
 -- 7. У каждой группы выведите альбом, который относится к наибольшему числу жанров
 select a.Id_collectives, (
                           select t.Id_album
@@ -244,16 +265,152 @@ select c.*
     ) t2
     inner join People p on p.Id_People = t1.Id_People
     where t1.Id_collectives = t2.Id_collectives and t1.`count`=t2.`max`
+    
 -- 12. Сформулируйте и напишите 3 запроса на изменение данных, которые будут полезны в вашем проекте
 	-- Завершить существование состава, указанной группы
+        update `Group`
+          set Date_Finish = sysdate()
+          where Id_collectives = 2 -- выбранный коллекив
+				and Date_Finish is null
+			--выпполнил но выдал значок предупреждения Incorrect date value: '2019-04-26 23:18:40' for column 'Date_Finish'
+-- ------------------------------------------------------------------------------------------------------------- --
     -- Всем людям добавить приставку ('the_' + первую букву группы + '_') к никнэйму в указанной группе
-    -- Изменить никнэйм самого вокалиста, указанной группы
+      update People as p
+	        inner join Group_People g_p on g_p.Id_people = p.Id_People
+            inner join `Group` g on g.`Id_Group` = g_p.`Id_Group`
+            inner join Collectives c on c.Id_collectives = g.Id_collectives
+      set p.Nickname =  concat_ws('_', 'The', substr(c.`name`,4,2), p.`Nickname`,'')
+            where g.Id_collectives = 1
+      
+	select @kk := instr('The Beatles', 'The '),
+			@k := case 
+                    when @kk > 0 then @kk + 3
+                    else 0
+		         end as p
+from (SELECT @k := 0) -- Error Code: 1248. Every derived table must have its own alias
+-- ------------------------------------------------------------------------------------------------------------- --
+    -- Изменить никнэйм самого частого вокалиста, указанной группы
+    update People as p set p.`Nickname`= p.`Nickname`
+           where p.`Id_People` = ( -- с in не работает error 1175
+								  select id_people
+                                   from (
+                                         select Id_collectives, max(roll) as `max`
+                                            from (
+												  select g_p.Id_people, g.Id_collectives, count(Id_Roles) as roll
+                                                   from Group_People g_p
+                                                  inner join `Group` g on g.`Id_Group` = g_p.`Id_Group`
+                                                  inner join People_Roles p_r on p_r.Id_People = g_p.Id_People
+                                                  where Id_Collectives = 1 and Id_Roles = 1
+                                                  group by Id_collectives, Id_people
+                                            ) t
+                                            group by Id_collectives
+                                    ) tt
+                                   inner join (
+                                              select g_p.Id_people, g.Id_collectives, count(Id_Roles) as roll
+												from Group_People g_p
+                                              inner join `Group` g on g.`Id_Group` = g_p.`Id_Group`
+                                              inner join People_Roles p_r on p_r.Id_People = g_p.Id_People
+                                              where Id_Collectives = 1 and Id_Roles = 1
+                                              group by Id_collectives, Id_people
+                                   ) t2 on t2.roll = tt.`max`
+                                   limit 1
+		   )
     
 -- 13. Сформулируйте и напишите 3 запроса на удаление данных, которые будут полезны в вашем проекте
-	-- Удалить составы групп длившиесЯ не больше года если за это время не было альбомов
-    -- Удалить людей бывших в одном составе
+	-- Удалить составы групп длившиесЯ не больше года, если за это время не было альбомов
+    create view Group_D_A_bool as
+    select Id_group
+     from (
+           select distinct g.Id_Group, g.Id_collectives, g.Date_Finish - Date_Start < 365/2 as `Dbool`, (
+                select 
+                      case
+                          when count(*) > 0 then 0 
+                          else 1 
+	    	          end 
+	    	     from(select distinct * from Album a) t 
+	    	     where t.Id_group = g.Id_Group
+                 ) as `Abool`
+            from `Group` g
+            inner join Album a on a.Id_collectives = g.Id_collectives
+    ) t
+    where Dbool =1 and Abool = 1
+
+    -- DELETE MULTI не получилось сделать
+    delete  g_p
+         from  `Group_People` as g_p
+         join Group_D_A_bool gda on gda.Id_group = g_p.Id_Group
+
+    delete  g
+		  from  `Group` as g
+          join Group_D_A_bool gda on gda.Id_group = g.Id_Group
+-- ------------------------------------------------------------------------------------------------------------- --
+    -- Удалить людей не входящих в группы или в авторы
+    create view P_extra as
+        select Id_People
+         from People p
+        inner join (
+                    select Id_People as Gb_people
+                    from (
+						select p.Id_People, (
+                                            select t.`count` > 0 
+                                            from (select Id_people, count(*) as `count` from Group_People group by Id_people) t 
+                                            where t.Id_people = p.Id_People
+                        ) as Gbool
+                        from People p
+                    ) t
+                    where Gbool is null
+    ) gb on gb.Gb_people = p.Id_People
+    inner join (
+                select Id_People as Ab_people
+                 from (
+					   select p.Id_People, (
+                                           select t.`count` > 0
+										   from (select Id_Author, count(*) as `count` from Author group by Id_Author) t 
+										   where t.Id_Author = p.Id_People
+                                            ) as Abool
+                        from People p
+				) t
+                where Abool is null
+    ) ab on ab.Ab_people = p.Id_People
+    
+	delete p_r
+          from People_Roles p_r
+          inner join P_extra p_e on p_e.Id_People = p_r.Id_People
+    
+    delete p
+          from People p
+          inner join P_extra p_e on p_e.Id_People = p.Id_People
+-- ------------------------------------------------------------------------------------------------------------- --
     -- Удалить самых непопулярных авторов каждой группе
+    create view Autor_min as
+        select t.Id_collectives, t.Id_Author
+		 from (
+			   select al.`Id_collectives`, a.Id_Author, count(*) as `count`
+			    from Author a
+			   inner join Compositions c on c.id = a.Id_compositions
+			   inner join Album_Compositions a_c on a_c.`Id_compositions` = c.id
+			   inner join Album al on al.`Id_Album` = a_c.`Id_album`
+			   group by al.`Id_collectives`, a.Id_Author
+		 ) t
+       join (
+             select Id_collectives, min(`count`) as `min`
+             from (
+                   select al.`Id_collectives`, a.Id_Author, count(*) as `count`
+                     from Author a
+                   inner join Compositions c on c.id = a.Id_compositions
+                   inner join Album_Compositions a_c on a_c.`Id_compositions` = c.id
+                   inner join Album al on al.`Id_Album` = a_c.`Id_album`
+                   group by al.`Id_collectives`, a.Id_Author
+		     ) tt
+             group by Id_collectives
+        ) m
+        where m.Id_collectives = t.Id_collectives and t.`count` = m.`min`
 
-
-
+         delete a
+               from Author a
+               inner join Autor_min a_m on a_m.Id_Author = a.Id_Author
+               
+        delete p
+              from People p
+              inner join P_extra p_e on p_e.Id_People = p.Id_People
 
